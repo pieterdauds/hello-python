66bced830a797022d3d11eb759e976f5ce94f171
Pieter Daud Satumalay <peter.crusties@gmail.com>
2022-02-10 13:25:30 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Simple Python Flask!"
+    return "Simple Python Flask PSY!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
